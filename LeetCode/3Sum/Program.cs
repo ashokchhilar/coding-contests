3d4c381bcddffcbe151c6cd4a12b68c87c4d17c9
Ashok Chhilar <ashok.get@gmail.com>
2017-11-29 19:09:43 -0800

    Two more puzzles

---
+++ LeetCode/3Sum/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3Sum
+{
+    public class Solution
+    {
+        public IList<IList<int>> ThreeSum(int[] nums)
+        {
+            IList<IList<int>> resultList = new List<IList<int>>();
+
+            List<int> n = new List<int> (nums);
+            n.Sort();
+
+            //find all the combinations
+            for(int i=0; i<n.Count; i++)
+            {
+                if (i > 0 && n[i] == n[i - 1]) continue;
+
+                for(int j=i+1; j<n.Count-1; j++)
+                {
+                    if (j > i + 1 && n[j] == n[j - 1]) continue;
+
+                    int toFind = -1*(n[i] + n[j]);
+                    if (toFind < n[j]) break;
+
+                    int searchResult = -1;
+                    if (toFind == n[j] && n[j] == n[j + 1]) searchResult = j + 1;
+                    else
+                        searchResult = n.BinarySearch(toFind);
+
+                    if(searchResult > j)
+                    {
+                        resultList.Add(new List<int>{n[i], n[j], n[searchResult]});
+                    }
+                }
+            }
+
+            return resultList;
+        }
+
+        static void Main(string[] args)
+        {
+            Solution sol = new Solution();
+            var result = sol.ThreeSum(new int[] { -1, 0, 1, 2, -1, -4 });
+            var result2 = sol.ThreeSum(new int[] {0,0,0 });
+            var result3 = sol.ThreeSum(new int[] { 0, 0 });
+            var result4 = sol.ThreeSum(new int[] { 0, 0, 10, 0 });
+            var result5 = sol.ThreeSum(new int[] { -1, 0, 0, 1 });
+        }
+    }
+}
---
+++ LeetCode/LargestCommonPrefix/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestCommonPrefix
+{
+    public class Solution
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            if (strs == null || strs.Length == 0) return "";
+
+            int minLen = strs.Min(x => x.Length);
+
+            //what can we do to the string
+            int i = 0;
+            while (i < minLen)
+            {
+                char ch = strs.First()[i];
+                if (strs.Any(x => x[i] != ch))
+                {
+                    break;
+                }
+                else i++;
+            }
+
+            return strs.First().Substring(0, i);
+        }
+
+        static void Main(string[] args)
+        {
+            string output = new Solution().LongestCommonPrefix(new string[] { "", "" });
+        }
+    }
+}
