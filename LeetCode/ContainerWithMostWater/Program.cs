f4a45caf601b71caa94ab04f68f6f41f6827ece5
Ashok Chhilar <ashok.get@gmail.com>
2017-11-29 16:21:40 -0800

    Solutions for multiple problems
    1. Container with Most water
    2. Regular Expressions Matching exercise

---
+++ LeetCode/ContainerWithMostWater/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ContainerWithMostWater
+{
+    public class Solution
+    {
+        public int MaxArea(int[] height)
+        {
+            int max = 0, l = 0, r = height.Length - 1;
+            while(l<r)
+            {
+                var minHeight = min(height[l], height[r]);
+                var area = minHeight * (r - l);
+
+                if (max < area) max = area;
+
+                if (minHeight == height[l]) l++;
+                else r--;
+            }
+            return max;
+        }
+
+        public int min(int a, int b)
+        {
+            return a < b ? a : b;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] input = new int[] { 1, 2, 1 };
+            var result = new Solution().MaxArea(input);
+        }
+    }
+}
--- LeetCode/RegularExpressionMatching/Program.cs
+++ LeetCode/RegularExpressionMatching/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RegularExpressionMatching
 {
@@ -10,11 +7,64 @@ namespace RegularExpressionMatching
     {
         public bool IsMatch(string s, string p)
         {
-            throw new NotImplementedException();
+            bool?[,] match = new bool?[p.Length+1, s.Length+1];
+
+            return recursiveSolution(0, 0, p, s, match);
         }
+
+        public bool recursiveSolution(int i, int j, string P, string T, bool?[,] match)
+        {
+            if (i == P.Length && j == T.Length)
+            {
+                match[i, j] = true;
+                return true;
+            }
+
+            if (i <= P.Length && j <= T.Length && match[i, j].HasValue) return match[i, j].Value;
+
+            if (i >= P.Length) return false;
+            if (j > T.Length) return false;
+
+            if(P[i] == '*')
+            {
+                int k = j;
+                if (recursiveSolution(i + 1, k, P, T, match)) { match[i, j] = true; return true; }
+
+                while (k < T.Length && (T[k++] == P[i-1] || P[i-1] == '.'))
+                {
+                    if (recursiveSolution(i + 1, k, P, T, match)) { match[i, j] = true; return true; }
+                }
+            }
+            else
+            {
+                //its a string
+                if (j < T.Length)
+                {
+                    if ((T[j] == P[i] || P[i] == '.') && recursiveSolution(i + 1, j + 1, P, T, match)) { match[i, j] = true; return true; }
+                }
+
+                if (i+1 < P.Length && P[i + 1] == '*')
+                    return recursiveSolution(i + 1, j, P, T, match);
+            }
+            match[i, j] = false;
+            return false;
+        }
+
 
         static void Main(string[] args)
         {
+            Solution sol = new Solution();
+            var match = sol.IsMatch("aa", "a");//→ false
+            var match2 = sol.IsMatch("aa", "aa") ;//→ true
+            var match3 = sol.IsMatch("aaa", "aa") ;//→ false
+            var match4 = sol.IsMatch("aa", "a*") ;//→ true
+            var match5 = sol.IsMatch("aa", ".*") ;//→ true
+            var match6 = sol.IsMatch("ab", ".*") ;//→ true
+            var match7 = sol.IsMatch("aab", "c*a*b") ;//→ true
+            var match8 = sol.IsMatch("a", "ab*");//→ true
+            var match9 = sol.IsMatch("bbbba", ".*a*a"); //true
+            var match10 = sol.IsMatch("ab", ".*.."); //true
+            var match11 = sol.IsMatch("aaaaaaaaaaaaab", "a*a*a*a*a*a*a*a*a*a*c");
         }
     }
 }
