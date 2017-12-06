ef91f9e567d0dc967a758ba65aa643c3265c3520
Ashok Chhilar <ashok.get@gmail.com>
2017-12-05 17:57:22 -0800

    trapping rain water

--- LeetCode/ReverseNodesInKGroups/Program.cs
+++ LeetCode/ReverseNodesInKGroups/Program.cs
@@ -14,10 +14,6 @@ namespace ReverseNodesInKGroups
     {
         public Tuple<ListNode, ListNode> ReverseWholeGroup(ListNode head, ListNode last)
         {
-            //Given this linked list: 1->2->3->4->5
-            //For k = 2, you should return: 2->1->4->3->5
-            //For k = 3, you should return: 3->2->1->4->5
-
             ListNode next = null;
             ListNode previous = null;
 
---
+++ LeetCode/TrappingRainWater/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TrappingRainWater
+{
+    public class Solution
+    {
+        public int Trap(int[] height)
+        {
+            Stack<int> wallStack = new Stack<int>();
+
+            int totalArea = 0;
+
+            for(int i=0; i<height.Length; i++)
+            {
+                while(wallStack.Count != 0 && height[wallStack.Peek()] < height[i])
+                {
+                    var top = wallStack.Pop();
+                    if (wallStack.Count == 0) break;
+
+                    int boundingheight = Math.Min(height[wallStack.Peek()], height[i]) - height[top];
+                    int boundingdistance = i - wallStack.Peek() - 1;
+                    var area = boundingheight * boundingdistance;
+                    totalArea += area;
+                }
+                wallStack.Push(i);
+            }
+            return totalArea;
+        }
+
+
+        public static void Main(string[] args)
+        {
+            int[] sample = new int[] {  };
+            Solution sol = new Solution();
+            var result = sol.Trap(sample);
+        }
+    }
+}
