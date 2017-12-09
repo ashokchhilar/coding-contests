e1a60c0620775d5647b9b18099cdb83d77c467e8
Ashok Chhilar <ashok.chhilar@live.com>
2017-12-09 10:42:30 -0800

    Find first missing positive

---
+++ LeetCode/FirstMissingPositive/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstMissingPositive
+{
+    public class Solution
+    {
+
+        public void swap(int i, int j, int[] num)
+        {
+            int temp = num[i];
+            num[i] = num[j];
+            num[j] = temp;
+        }
+
+        public int FirstMissingPositive(int[] nums)
+        {
+            if (nums == null || nums.Length == 0) return 1;
+
+            int i = 0;
+            while (i < nums.Length)
+            {
+                if (nums[i] <= nums.Length && nums[i] > 0 && nums[i] != i + 1 && nums[nums[i] - 1] != nums[i])
+                {
+                    swap(nums[i] - 1, i, nums);
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            int j = 0;
+
+            while (j < nums.Length && nums[j] == j + 1) j++;
+            return j + 1;
+        }
+
+        static void Main(string[] args)
+        {
+            Solution sol = new Solution();
+            int result = sol.FirstMissingPositive(new int[] {1,1});
+        }
+    }
+}
---
+++ LeetCode/SearchInRotatedSortedArray/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchInRotatedSortedArray
+{
+    public class Program
+    {
+        public int LocateSmallest(int[] nums)
+        {
+            //Pivot will be the smallest value;
+            int half = nums.Length / 2;
+            int start = 0;
+            int end = 
+
+            throw new NotImplementedException();
+        }
+
+        public int Search(int[] nums, int target)
+        {
+
+            throw new NotImplementedException();
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
