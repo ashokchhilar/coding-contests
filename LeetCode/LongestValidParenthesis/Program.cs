8b4fdf4d12d821fc02fc08be826f16ca04abee3c
Ashok Chhilar <ashok.chhilar@live.com>
2017-12-10 12:46:14 -0800

    Longest valid parenthesis

---
+++ LeetCode/LongestValidParenthesis/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestValidParenthesis
+{
+    public class Solution
+    {
+        public int LongestValidParentheses(string s)
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(-1);
+            int max = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                else
+                {
+                    stack.Pop();
+                    if (stack.Count == 0) stack.Push(i);
+                    else
+                    {
+                        int len = i - stack.Peek();
+                        if (len > max) max = len;
+                    }
+                }
+            }
+            return max;
+        }
+
+        static void Main(string[] args)
+        {
+            string p = "((())()";
+            Solution sol = new Solution();
+            int result = sol.LongestValidParentheses(p);
+        }
+    }
+}
