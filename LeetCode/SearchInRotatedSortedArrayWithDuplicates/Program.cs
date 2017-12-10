52655ca442af887f5cf393a6580343b2c0e1f0a0
Ashok Chhilar <ashok.chhilar@live.com>
2017-12-10 10:52:51 -0800

    Search in rotated array various versions

--- LeetCode/SearchInRotatedSortedArrayWithDuplicates/Program.cs
+++ LeetCode/SearchInRotatedSortedArrayWithDuplicates/Program.cs
@@ -4,21 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SearchInRotatedSortedArray
+namespace SearchInRotatedSortedArrayWithDuplicates
 {
     public class Solution
     {
-        public int Search(int[] nums, int target)
-        {
-            int start = 0;
-            int end = nums.Length - 1;
 
-            while(start <= end)
+        public bool Search(int[] nums, int target, int start, int end)
+        {
+            if (start > end) return false;
+
+            int half = (end - start) / 2 + start;
+            if (target == nums[half]) return true;
+
+            if (nums[start] == nums[end])//can't determine the rotation point, search both paths
             {
-                int half = (end - start) / 2 + start;
-                if (nums[half] == target) return half;
-                if (nums[start] == target) return start;
-                if (nums[end] == target) return end;
+                return Search(nums, target, start, half - 1) || Search(nums, target, half + 1, end);
+            }
+
+            while (start <= end)
+            {
+                half = (end - start) / 2 + start;
+                if (nums[half] == target) return true;
+                if (nums[start] == target) return true;
+                if (nums[end] == target) return true;
+
+                if (nums[start] == nums[half] && nums[half] == nums[end]) { start++; end--; }
 
                 else if (nums[start] > nums[half]) //rotation is in first half & second half is purely an increasing sequence
                 {
@@ -43,15 +53,21 @@ namespace SearchInRotatedSortedArray
                 }
             }
 
-            return -1;
+            return false;
+        }
+
+        public bool Search(int[] nums, int target)
+        {
+            if (nums == null || nums.Length == 0) return false;
+            return Search(nums, target, 0, nums.Length - 1);
         }
 
         static void Main(string[] args)
         {
             Solution sol = new Solution();
-            int[] nums = new int[] { 4, 5, 6, 6, 7, 0, 1, 2 };
+            int[] nums = new int[] { 1 };
 
-            int result = sol.Search(nums, 6);
+            bool result = sol.Search(nums, 3);
             result = sol.Search(nums, 1);
             result = sol.Search(nums, 3);
         }
