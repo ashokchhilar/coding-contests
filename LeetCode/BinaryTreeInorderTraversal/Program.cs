d769041486978a435a1586e7b6d0b4d92c4266d3
Ashok Chhilar <ashok.chhilar@live.com>
2017-12-10 16:06:18 -0800

    Solution to problem Search For Range and Letter Combinations

---
+++ LeetCode/BinaryTreeInorderTraversal/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeInorderTraversal
+{
+    /**
+     * Definition for a binary tree node. */
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+     
+    public class Solution
+    {
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+
+            throw new NotImplementedException();
+        }
+
+    static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ LeetCode/SearchForRange/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchForRange
+{
+    public class Solution
+    {
+        /// <summary>
+        /// Arrange lo and high to match range
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public int[] SearchRange(int[] nums, int target)
+        {
+            int lo = 0;
+            int hi = nums.Length - 1;
+
+            while (lo <= hi)
+            {
+                if (nums[lo] == target && nums[hi] == target) return new int[] { lo, hi };
+                int mid = (hi - lo) / 2 + lo;
+
+                if(nums[mid] == target)
+                {
+                    int midCopy = mid;
+                    //search lower and higher
+                    while (mid > lo && nums[lo]!=target)
+                    {
+                        var mid2 = (mid - lo) / 2 + lo;
+                        if (nums[mid2] != target) lo = mid2 + 1;
+                        else mid = mid2;
+                    }
+
+                    mid = midCopy;
+                    while(mid<hi && nums[hi] != target)
+                    {
+                        var mid2 = (hi - mid) / 2 + mid;
+                        if (mid == mid2) hi--;
+
+                        else if (nums[mid2] != target) hi = mid2 - 1;
+                        else mid = mid2;
+                    }
+
+                    return new int[] { lo, hi };
+                }
+
+                if (target > nums[mid]) lo = mid + 1;
+                else hi = mid - 1;
+            }
+
+            return new int[] { -1, -1 };
+        }
+
+        static void Main(string[] args)
+        {
+            Solution sol = new Solution();
+            int[] nums = new int[] {1,2,3,8,8,8,9,9,10};
+
+            var result = sol.SearchRange(nums, 8);
+        }
+    }
+}
